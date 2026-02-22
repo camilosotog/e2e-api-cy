390868712da089a5acaccfae19385bfe6cdecd5c
Camilo Soto <occamilo97@gmail.com>
2026-02-21 20:35:50 -0500

    Type: feat desc: add data test with interfaces implements

---
+++ cypress/e2e/ui/data/module.data.ts
@@ -0,0 +1,9 @@
+import { Module } from "../interfaces/module.interface";
+
+export const homeModule: Module = {
+    name: "Home (current)"
+}
+
+export const cartModule: Module = {
+    name: "Cart"
+}
---
+++ cypress/e2e/ui/data/user.data.ts
@@ -0,0 +1,6 @@
+import { User } from "../interfaces/user.interface";
+
+export const testUser: User = {
+    username: "test",
+    password: "test"
+}
